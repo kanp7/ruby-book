7488554d359222834ecbde5d4dad0974e18c5346
kanp7 <tatsuya715tk@gmail.com>
2020-06-10 22:23:14 +0900

    Add calendar作成

---
+++ lib/calendar.rb
@@ -0,0 +1,24 @@
+require 'date'
+
+def display_calendar
+	today = Date.today
+	first_day = Date.new(today.year, today.month, 1)
+	last_day = Date.new(today.year, today.month, -1)
+
+	first_wday = first_day.wday
+	
+
+  days = (1..last_day.day).map{|n| n.to_s.rjust(2)}
+
+	days = Array.new(first_wday, ' ').push(days).flatten.each_slice(7).to_a
+
+	puts today.strftime("%B %Y").center(21)
+	puts "Su Mo Tu We Th Fr St"
+
+	days.each do |week|
+		puts week.join(' ')
+	end
+end
+
+
+puts display_calendar
